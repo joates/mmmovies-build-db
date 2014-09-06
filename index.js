7b107fc01a124f6dc24b52096d216036762a22b4
joates <github@joat.es>
2014-09-06 23:09:39 +0100

    extract movie releases for entire month (instead of weekly)

--- index.js
+++ index.js
@@ -4,10 +4,7 @@ if (! module.parent) {
   app(require('./lib/cli')(process.argv.slice(2)), function(err, data) {
     if (err) throw err
     process.stdout.on('error', process.exit)
-    data.pipe(process.stdout).on('end', function() {
-      //console.log('end program')
-      //process.exit()
-    })
+    data.pipe(process.stdout)
   })
 }
 
